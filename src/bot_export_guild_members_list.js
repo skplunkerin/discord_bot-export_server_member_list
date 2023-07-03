0e5de78e5bc2ad987ba04e916a82d831df5d5f49
Skplunkerin <skplunkerin@pm.me>
2023-07-03 11:44:25 -0600

    Experiment with limiting member lookup to specific roles (#1)
    * Experiment with limiting member lookup to specific roles
    
    * * Cleanup and simplify `client.once(Events.ClientReady, ...)` logic
    
    * Get member lookup for role working (miraculously)
    
    * Remove discriminator data logic since that won't work as expected
    
    * Update `README.md` and `.env.sample` with new Role ID's list info
    
    * Delete discriminator sample file
    
    * Break out member loop to it's own `getCsvDataForMembers()` function
    
    * Break out file creation to it's own `createCsvFile()` function
    
    * Break out roleId's async loop to it's own `createCsvFilesForRoleIds()`
      function

--- src/bot_export_guild_members_list.js
+++ src/bot_export_guild_members_list.js
@@ -7,90 +7,155 @@ const client = new Client({
   intents: [GatewayIntentBits.GuildMembers],
 });
 
+/**
+ * Loops through a Discord Collection of GuildMember objects and returns them as
+ * a CSV string with the following headers:
+ *   ```
+ *   userId,userIsBot,hasMigratedUsername,guildMemberNickname,userGlobalName,userUsername,userDiscriminator,pendingGuildMember,guildMemberRoles
+ *   ```
+ *
+ * @param {Collection<string, GuildMember>} members discord Collection of GuildMember objects
+ * @returns {string} members in as a string of CSV data
+ */
+function getCsvDataForMembers(members) {
+  let csvData =
+    "userId,userIsBot,hasMigratedUsername,guildMemberNickname,userGlobalName,userUsername,userDiscriminator,pendingGuildMember,guildMemberRoles\n";
+  let membersProcessed = 0;
+  members.forEach((member) => {
+    const {
+      nickname,
+      pending,
+      _roles,
+      user: { id, bot, username, discriminator },
+    } = member;
+
+    // attempting to get the user.globalName value (introduced by the
+    // Discord username migration) but it's not yet available in the
+    // Discord.js library...
+    // - they literally just added it 9 hours ago (2023/06/08 @ 12:16PM MDT)
+    //   after username migration has been happening already, and it's not
+    //   yet available as a version
+    //   https://github.com/discordjs/discord.js/pull/9512)
+    //
+    // console.log("member.user:", member.user);
+    //// const userGlobalName = member.user.globalName;
+    const userGlobalName = "";
+    //// const user = await member.user.fetch(true);
+    //// const userGlobalName = user.globalName;
+    //// console.log("user:", user);
+
+    // if discriminator is "0", then the user has migrated their username
+    const hasMigratedUsername = discriminator === "0";
+    // Remove commas from nickname
+    const sanitizedNickname = nickname ? nickname.replace(/,/g, "") : "";
+    // Join roles array into a comma-separated string and wrap in quotes to
+    // prevent commas from breaking the CSV format
+    const sanitizedRoles = _roles ? `"${_roles.join(",")}"` : "";
+
+    // create the csvData row for this member
+    csvData += `${id},${bot},${hasMigratedUsername},${sanitizedNickname},${userGlobalName},${username},${discriminator},${pending},${sanitizedRoles}\n`;
+    membersProcessed++;
+  });
+  console.log("\tmembers processed:", membersProcessed);
+  console.log();
+  return csvData;
+}
+
+/**
+ * Creates a CSV file with the given CSV data using the following filename:
+ *
+ *  ```
+ *  guildId_{discordGuildId}-roleName_{discordRoleName}-roleId_{discordRoleId}-members_list_{YYYY-MM-DDThh:mm:ssZ}.csv
+ *  ```
+ *
+ * NOTE: if `DEBUG` mode is set to `true` in the .env file, the CSV data is
+ * logged to the console instead of written to a file.
+ *
+ * @param {string} guildId is the Discord Guild (server) ID for the CSV data
+ * @param {string} roleId is the Discord server Role ID for the CSV data
+ * @param {string} roleName is the Discord server Role name for the CSV data
+ * @param {string} csvData is a string of CSV data to save to a file
+ */
+async function createCsvFile(guildId, roleId, roleName, csvData) {
+  const formattedDate = new Date().toISOString(); // Format date as 'YYYY-MM-DDThh:mm:ssZ'
+  const fileName = `guildId_${guildId}-roleName_${roleName}-roleId_${roleId}-members_list_${formattedDate}.csv`;
+  if (process.env.DEBUG) {
+    // if in debug mode, just log the csvData to the console
+    console.log(`${fileName} data:`);
+    console.log(csvData);
+  } else {
+    // if not in debug mode, write the csvData to a file for later
+    // processing
+    fs.writeFile(fileName, csvData, (err) => {
+      if (err) throw err;
+      console.log("\tCSV file created!");
+    });
+  }
+}
+
+/**
+ * Creates a CSV file for each Discord Role ID containing the Guild Members with
+ * that role.
+ * NOTES:
+ *   - If the role ID has no members, a CSV file is still created.
+ *   - If the role ID is not found, no CSV file is created.
+ *
+ * @param {Guild} guild is the Discord Guild object
+ * @param {string[]} roleIds is an array of Discord Role IDs from the .env file
+ */
+async function createCsvFilesForRoleIds(guild, roleIds) {
+  console.log("createCsvFilesForRoleIds() called...");
+  for (const roleId of roleIds) {
+    try {
+      console.log("\troleId:", roleId);
+      // NOTE: this should work but isn't, not sure if it's a permission
+      // issue, a bug, or I'm doing something wrong:
+      // guild.roles.cache.get(roleId)
+      //
+      // this successfully gets the role but not the role member data; see below
+      // `guild.members.fetch()` which seems to populate it just for this role.
+      // (so intuitive, right?)
+      const role = await guild.roles.fetch(roleId);
+      if (role) {
+        console.log(`\tRole: ${role.name}`);
+        // NOTE: `role.members` has no data unless `guild.members.fetch()` is
+        // called first:
+        await guild.members.fetch();
+        const members = role.members;
+        console.log("\tMembers with role:", members.size);
+        const csvData = getCsvDataForMembers(members);
+        await createCsvFile(
+          process.env.DISCORD_GUILD_ID,
+          roleId,
+          role.name,
+          csvData
+        );
+      } else {
+        console.log("\tRole not found.");
+      }
+    } catch (error) {
+      console.error("Error fetching members:", error);
+    }
+  }
+  console.log("...finished createCsvFilesForRoleIds()");
+}
+
 client.once(Events.ClientReady, async () => {
   console.log(process.env.DEBUG ? "Debugging...\n" : "Creating CSV file...\n");
-
   const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID);
-
-  // Load the array of discriminators from the JSON file
-  const discriminatorData = fs.readFileSync(
-    path.join(__dirname, "discord_user_discriminators.json"),
-    "utf8"
-  );
-  const discriminatorArray = JSON.parse(discriminatorData);
-
-  // Use guild.members.fetch to make sure all members are cached first
-  await guild.members
-    .fetch({ withPresences: true })
-    .then(async (fetchedMembers) => {
-      console.log("total members:", fetchedMembers.size);
-
-      let csvData =
-        "userId,userIsBot,hasMigratedUsername,guildMemberNickname,userGlobalName,userUsername,userDiscriminator,pendingGuildMember,guildMemberRoles\n";
-      let membersProcessed = 0;
-      let membersFound = 0;
-      for (const member of fetchedMembers.values()) {
-        const {
-          nickname,
-          pending,
-          _roles,
-          user: { id, bot, username, discriminator },
-        } = member;
-
-        // attempting to get the user.globalName value (introduced by the
-        // Discord username migration) but it's not yet available in the
-        // Discord.js library...
-        // - they literally just added it 9 hours ago (2023/06/08 @ 12:16PM MDT)
-        //   after username migration has been happening already, and it's not
-        //   yet available as a version
-        //   https://github.com/discordjs/discord.js/pull/9512)
-        //
-        // console.log("member.user:", member.user);
-        //// const userGlobalName = member.user.globalName;
-        const userGlobalName = "";
-        //// const user = await member.user.fetch(true);
-        //// const userGlobalName = user.globalName;
-        //// console.log("user:", user);
-
-        // if discriminator is "0", then the user has migrated their username
-        const hasMigratedUsername = discriminator === "0" ? true : false;
-
-        // Add user if the first 4 digits of `id` exist in the
-        // `discriminatorArray`
-        const firstFourDigits = id.slice(0, 4);
-        if (discriminatorArray.includes(firstFourDigits)) {
-          membersFound++;
-          const sanitizedNickname = nickname ? nickname.replace(/,/g, "") : ""; // Remove commas from nickname
-          const sanitizedRoles = _roles ? `"${_roles.join(",")}"` : ""; // Join roles array into a comma-separated string
-          csvData += `${id},${bot},${hasMigratedUsername},${sanitizedNickname},${userGlobalName},${username},${discriminator},${pending},${sanitizedRoles}\n`;
-        }
-        membersProcessed++;
-      }
-
-      console.log("members processed:", membersProcessed);
-      console.log("members found in discriminatorArray:", membersFound);
-      console.log();
-
-      if (process.env.DEBUG) {
-        // if in debug mode, just log the csvData to the console
-        console.log(csvData);
-        console.log("Finished.");
-        process.exit();
-      } else {
-        // if not in debug mode, write the csvData to a file for later
-        // processing
-        const formattedDate = new Date().toISOString(); // Format date as 'YYYY-MM-DDThh:mm:ssZ'
-        fs.writeFile(
-          `guild-${process.env.DISCORD_GUILD_ID}-members_list-${formattedDate}.csv`,
-          csvData,
-          (err) => {
-            if (err) throw err;
-            console.log("CSV file created!");
-            process.exit();
-          }
-        );
-      }
-    });
+  await createCsvFilesForRoleIds(
+    guild,
+    process.env.DISCORD_ROLES.split(",")
+  ).then(() => {
+    if (process.env.DEBUG) {
+      console.log("Finished");
+    } else {
+      console.log("Files created");
+    }
+    // Can't close this as it'll close before the last role file finishes
+    // writing everything.
+    // process.exit();
+  });
 });
 
 client.login(process.env.DISCORD_APP_TOKEN);
