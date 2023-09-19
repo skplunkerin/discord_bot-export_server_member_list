cc4946b35b3a368d1b0902db74a9507d8d377171
skplunkerin <skplunkerin@pm.me>
2023-09-19 07:28:11 -0600

    * Remove unnecessary copied code
    * Update `.gitignore` for json file location
    
    * Update `client.once` function to correctly search for each member,
      check if their roles are from the discordRoles table, and outputs SQL
      upsert query for the `profileId`/`discordRoleId` relationship
    
    * TODO:
      - save upsert queries to `.sql` file
      - remove unneeded logging

--- src/get_discord_roles_for_member_ids.js
+++ src/get_discord_roles_for_member_ids.js
@@ -26,130 +26,59 @@ function readFile(filePath) {
   }
 }
 
-// TODO: remove this test code. [skplunkerin]
-// Parse JSON data into an array of User objects
-const users = JSON.parse(readFile("profile_ids-server_member_ids.json"));
-console.log("users:", users);
-// Loop through the users array and update each object
-for (const user of users) {
-  user.roles = ["string1", "string2"];
-}
-console.log("users:", users);
-
 /**
- * Loops through array of user objects (containing id and discordUserId),
- * searches Discord Guild for roles belonging to the user, and returns an
- * updated users array containing found roles array added to each user object.
- *
- * @param {Array<{ id: number, discordUserId: string }>} users - The array of user objects.
- * @returns {Array<{ id: number, discordUserId: string, roles?: string[] }>} - Updated array of user objects.
+ * Loops through array of user objects (containing id (profileId) and
+ * discordUserId), searches Discord Guild for roles belonging to this user,
+ * checks if the found user has any Discord roles that match a list of known
+ * roles, and if found saves a SQL upsert query for the `profileId` and
+ * `discordRoleId` relationship.
  */
-function findRolesForUsers(users) {
-  let processedCount = 0;
-  users.forEach((user) => {
-    const { id, discordUserId } = user;
-    processedCount++;
-  });
-  console.log("\tusers found:", processedCount);
-  console.log();
-  return csvData;
-}
+client.once(Events.ClientReady, async () => {
+  const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID);
 
-/**
- * Creates a CSV file with the given CSV data using the following filename:
- *
- *  ```
- *  guildId_{discordGuildId}-roleName_{discordRoleName}-roleId_{discordRoleId}-members_list_{YYYY-MM-DDThh:mm:ssZ}.csv
- *  ```
- *
- * NOTE: if `DEBUG` mode is set to `true` in the .env file, the CSV data is
- * logged to the console instead of written to a file.
- *
- * @param {string} guildId is the Discord Guild (server) ID for the CSV data
- * @param {string} roleId is the Discord server Role ID for the CSV data
- * @param {string} roleName is the Discord server Role name for the CSV data
- * @param {string} csvData is a string of CSV data to save to a file
- */
-async function createCsvFile(guildId, roleId, roleName, csvData) {
-  const formattedDate = new Date().toISOString(); // Format date as 'YYYY-MM-DDThh:mm:ssZ'
-  const fileName = `guildId_${guildId}-roleName_${roleName}-roleId_${roleId}-members_list_${formattedDate}.csv`;
-  if (process.env.DEBUG) {
-    // if in debug mode, just log the csvData to the console
-    console.log(`${fileName} data:`);
-    console.log(csvData);
-  } else {
-    // if not in debug mode, write the csvData to a file for later
-    // processing
-    fs.writeFile(fileName, csvData, (err) => {
-      if (err) throw err;
-      console.log("\tCSV file created!");
-    });
-  }
-}
+  // Parse JSON data into an array of User objects and discordRoles array
+  const discordRoles = JSON.parse(readFile("jsons/discord_role_ids.json"));
+  const users = JSON.parse(
+    readFile("jsons/profile_ids-server_member_ids.json")
+  );
 
-/**
- * Creates a CSV file for each Discord Role ID containing the Guild Members with
- * that role.
- * NOTES:
- *   - If the role ID has no members, a CSV file is still created.
- *   - If the role ID is not found, no CSV file is created.
- *
- * @param {Guild} guild is the Discord Guild object
- * @param {string[]} roleIds is an array of Discord Role IDs from the .env file
- */
-async function createCsvFilesForRoleIds(guild, roleIds) {
-  console.log("createCsvFilesForRoleIds() called...");
-  for (const roleId of roleIds) {
+  // loop through the users from the JSON file
+  for (const user of users) {
+    const { id: profileId, discordUserId } = user;
+    console.log("Profile.id:", profileId);
+    console.log("Profile.discordUserId:", discordUserId);
+    // get the member from the Discord Guild
     try {
-      console.log("\troleId:", roleId);
-      // NOTE: this should work but isn't, not sure if it's a permission
-      // issue, a bug, or I'm doing something wrong:
-      // guild.roles.cache.get(roleId)
-      //
-      // this successfully gets the role but not the role member data; see below
-      // `guild.members.fetch()` which seems to populate it just for this role.
-      // (so intuitive, right?)
-      const role = await guild.roles.fetch(roleId);
-      if (role) {
-        console.log(`\tRole: ${role.name}`);
-        // NOTE: `role.members` has no data unless `guild.members.fetch()` is
-        // called first:
-        await guild.members.fetch();
-        const members = role.members;
-        console.log("\tMembers with role:", members.size);
-        const csvData = getCsvDataForMembers(members);
-        await createCsvFile(
-          process.env.DISCORD_GUILD_ID,
-          roleId,
-          role.name,
-          csvData
-        );
-      } else {
-        console.log("\tRole not found.");
+      const member = await guild.members.fetch(discordUserId);
+      console.log("\tfound member.user.username:", member.user.username);
+      // loop through member roles and if the role exists in `discordRoles`
+      // create the upsert SQL query for it:
+      for (const roleId of member._roles) {
+        if (discordRoles[roleId]) {
+          console.log("\tfound roleId:", roleId);
+          console.log(
+            "\tdiscordRoles[roleId].name:",
+            discordRoles[roleId].name
+          );
+          // TODO: save (don't log) the upsert SQL queries for this role in a
+          // file. [skplunkerin]
+          // format: `VALUES (:discord_role_id, :profile_id)`
+          console.log(`-- Set Profile Discord roles for ${member.user.username}
+INSERT INTO "public"."_DiscordRoleToProfile" ("A", "B")
+VALUES (${discordRoles[roleId].discordRoleId}, ${profileId})
+ON CONFLICT ("A", "B") DO NOTHING
+;`);
+        }
       }
     } catch (error) {
-      console.error("Error fetching members:", error);
+      console.error("Error fetching member:", error);
+      console.log("-----");
+      break;
     }
+    console.log("-----");
   }
-  console.log("...finished createCsvFilesForRoleIds()");
-}
 
-client.once(Events.ClientReady, async () => {
-  console.log(process.env.DEBUG ? "Debugging...\n" : "Creating CSV file...\n");
-  const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID);
-  await createCsvFilesForRoleIds(
-    guild,
-    process.env.DISCORD_ROLES.split(",")
-  ).then(() => {
-    if (process.env.DEBUG) {
-      console.log("Finished");
-    } else {
-      console.log("Files created");
-    }
-    // Can't close this as it'll close before the last role file finishes
-    // writing everything.
-    // process.exit();
-  });
+  process.exit();
 });
 
 client.login(process.env.DISCORD_APP_TOKEN);
