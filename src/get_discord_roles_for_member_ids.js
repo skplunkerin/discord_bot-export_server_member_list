feed2f7e15e0c15379f6ac19f0a0691659a1b0a5
Skplunkerin <skplunkerin@pm.me>
2023-09-22 07:47:20 -0600

    Add script to get Discord roles for an array of Member ID's  (#2)
    * Add script to get Discord roles for an array of Member ID's
    
    * Update `.gitignore` for new JSON file used in new script
    
    * Update `README.md` for new file and old file name change
    
    * Increase package.json version and update description
    
    * Rename `bot_export_guild_members_list.js` to `export_guild_members_list.js`
    
    * WIP: Create new script `get_discord_roles_for_member_ids.js`
    
    * * Remove unnecessary copied code
    
    * Update `.gitignore` for json file location
    
    * Update `client.once` function to correctly search for each member,
      check if their roles are from the discordRoles table, and outputs SQL
      upsert query for the `profileId`/`discordRoleId` relationship
    
    * TODO:
      - save upsert queries to `.sql` file
      - remove unneeded logging
    
    * * Add `*.sql` to `.gitignore`
    
    * Update `get_discord_roles_for_member_ids.js`:
      - update logs formatting and content
      - save SQL upsert queries to file instead of in logs
    
    * * Include role name comment for each upsert query saved to file
    
    * * Add `*.log` to `.gitignore`
    
    * Fix caught errors from unknown Discord members to be logged to error
      file and to continue the loop
    
    * * Add example format of read files

---
+++ src/get_discord_roles_for_member_ids.js
@@ -0,0 +1,132 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.GuildMembers],
+});
+
+/**
+ * Finds the local file with the given name and returns the file data as a
+ * string.
+ *
+ * @param {string} filePath - The path to the JSON file to read.
+ * @returns {string} - The file data as a string.
+ */
+function readFile(filePath) {
+  try {
+    const absolutePath = path.resolve(__dirname, filePath);
+    // Read the file as text
+    const fileData = fs.readFileSync(absolutePath, "utf-8");
+    return fileData;
+  } catch (error) {
+    console.error("Error reading file:", error);
+    throw error;
+  }
+}
+
+/**
+ * Loops through array of user objects (containing id (profileId) and
+ * discordUserId), searches Discord Guild for roles belonging to this user,
+ * checks if the found user has any Discord roles that match a list of known
+ * roles, and if found saves a SQL upsert query for the `profileId` and
+ * `discordRoleId` relationship.
+ */
+client.once(Events.ClientReady, async () => {
+  const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID);
+  const formattedDate = new Date().toISOString(); // Format date as 'YYYY-MM-DDThh:mm:ssZ'
+  const fileName = `upsert_profile_discord_roles-${formattedDate}.sql`;
+  const errsFileName = `errors_found-${formattedDate}.log`;
+
+  // Initialize the file with a header
+  fs.writeFileSync(
+    fileName,
+    `/**
+  * NOTE: The profile-to-discordRole association format
+  * for table \`_DiscordRoleToProfile\` \`("A", "B")\` is:
+  * - \`VALUES (:discord_role_id, :profile_id)\`
+  */\n`
+  );
+
+  // Parse JSON data into an array of User objects and discordRoles array
+  // Example file format:
+  // {
+  //   "111111111111111111": { // Discord Server Role.id
+  //     "discordRoleId": 1, // discordRole.id
+  //     "name": "Some Role" // discordRole.name
+  //   }
+  // }
+  const discordRoles = JSON.parse(readFile("jsons/discord_role_ids.json"));
+  // Example file format:
+  // [
+  //   {
+  //     "id": 1, // Profile.id
+  //     "discordUserId": "222222222222222222" // Discord User.id
+  //   }
+  // ]
+  const users = JSON.parse(
+    readFile("jsons/profile_ids-server_member_ids.json")
+  );
+
+  // loop through the users from the JSON file
+  for (const user of users) {
+    const { id: profileId, discordUserId } = user;
+    console.log("Profile.id:", profileId);
+    console.log("Profile.discordUserId:", discordUserId);
+    // get the member from the Discord Guild
+    try {
+      const member = await guild.members.fetch(discordUserId);
+      console.log("found member.user.username:", member.user.username);
+      // Append the username to the file
+      fs.appendFileSync(
+        fileName,
+        `/***** ${member.user.username} Profile DiscordRoles SQL *****/\n`
+      );
+
+      // loop through member roles and if the role exists in `discordRoles`
+      // create the upsert SQL query for it:
+      for (const roleId of member._roles) {
+        if (discordRoles[roleId]) {
+          console.log(
+            `\tfound roleId: ${roleId}\t(${discordRoles[roleId].name})`
+          );
+          // Append the SQL query to file
+          fs.appendFileSync(
+            fileName,
+            // profile/discordRole format:
+            //   `VALUES (:discord_role_id, :profile_id)`
+            `-- ${discordRoles[roleId].name}
+INSERT INTO "public"."_DiscordRoleToProfile" ("A", "B")
+VALUES (${discordRoles[roleId].discordRoleId}, ${profileId})
+ON CONFLICT ("A", "B") DO NOTHING
+;\n`
+          );
+        }
+      }
+      // Append the username to the file
+      fs.appendFileSync(
+        fileName,
+        `/***** End queries for ${member.user.username} *****/\n`
+      );
+    } catch (error) {
+      fs.appendFileSync(
+        errsFileName,
+        `Profile.id: ${profileId}
+Profile.discordUserId: ${discordUserId}
+Error fetching member: ${error}
+\tmessage:\t${error.rawError.message}
+\tcode:\t${error.code}
+\tstatus:\t${error.status}
+\tmethod:\t${error.method}
+\turl:\t${error.url}\n\n`
+      );
+      console.error("Error fetching member:", error);
+    }
+    console.log("-----");
+  }
+
+  process.exit();
+});
+
+client.login(process.env.DISCORD_APP_TOKEN);
